91dd48471d4a29a4a289e6b9126943e7645e0f71
Prashant Kumar <prashantkumar102369@gmail.com>
2026-06-30 07:47:13 +0530

    registered home and inspect routes

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,24 @@ from app.config.config import Config
 load_dotenv()
 
 def create_app():
+    
+# declare the Flask App:
     app = Flask(__name__,
                 static_folder='static',
                 template_folder='templates'
             )
+# configure the config file
     app.config.from_object(Config)
+    
+# register blueprints:
+
+    # add auth_home route
+    from app.routes.auth_home import home_auth
+    app.register_blueprint(home_auth)
+    
+    # add auth_inspect route
+    from app.routes.auth_inspect import inspect_auth
+    app.register_blueprint(inspect_auth)
+    
     
     return app
