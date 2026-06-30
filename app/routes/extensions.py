fa3310d8e21940199e557a34fd25a3f92bbbd14f
Prashant Kumar <prashantkumar102369@gmail.com>
2026-06-30 07:48:30 +0530

    little changes

--- app/routes/extensions.py
+++ app/routes/extensions.py
@@ -9,8 +9,8 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 # Load environment variables from the .env file in the app directory
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+# load_dotenv(dotenv_path)
 
 print('Inside extensions')
 
