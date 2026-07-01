3a627b2a6fc0040e2064a31d19cc72d4618ea0ba
Prashant Kumar <prashantkumar102369@gmail.com>
2026-07-01 16:30:37 +0530

    small changes

--- app/routes/auth_inspect.py
+++ app/routes/auth_inspect.py
@@ -1,3 +1,7 @@
 from .extensions import *
 
 inspect_auth = Blueprint('inspect_auth', __name__)
+
+@inspect_auth.route('/inspect', methods=['POST', 'GET'])
+def inspect():
+    return render_template('auth/inspect.html')
